818df31562b8dbe2cdb50783d257ebe521244330
Sergei Aslanov <aslanovsergey@gmail.com>
2024-03-11 13:32:48 +0300

    machar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const obj = {};
+
+    for (const char of str) {
+        obj[char] = (obj[char] || 0) + 1
+    }
+
+    // Descending sort b - a, asc a - b
+    const sortedChars = Object.entries(obj).sort((a, b) =>  b[1] - a[1] );
+    // Also we can use for in loop to find max char
+
+    return sortedChars[0][0]
+}
 
 module.exports = maxChar;

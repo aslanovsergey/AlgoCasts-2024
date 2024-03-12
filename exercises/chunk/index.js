9045c208e5dec4b6577e0d96e9acbffa298c53b8
Sergey Aslanov <aslanovsergey@gmail.com>
2024-03-12 12:55:29 +0300

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// O(n)
+function chunk(array, size) {
+  const result = [];
+
+  for (const item of array) {
+    const lastItem = result[result.length - 1];
+
+    if (lastItem && lastItem.length < size) {
+      lastItem.push(item);
+    } else {
+      result.push([item]);
+    }
+  }
+
+  return result;
+}
 
 module.exports = chunk;
+
+// O(a*b)
+// function chunk(array, size) {
+//     const result = [];
+
+//     while (array.length) {
+//       const arr = [];
+//       for (let i = size; i > 0; i--) {
+//         arr.push(array.shift());
+//       }
+//       result.push(arr);
+//     }
+
+//     return result;
+//   }
+
+// O(n)?
+// function chunk(array, size) {
+//     const result = [];
+//     let startIndex = 0;
+
+//     while (startIndex < array.length) {
+//       const endIndex = startIndex + size;
+//       const chunk = array.slice(startIndex, endIndex);
+//       result.push(chunk);
+//       startIndex = endIndex;
+//     }
+
+//     return result;
+//   }

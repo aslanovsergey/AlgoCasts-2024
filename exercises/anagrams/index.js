1b62253630462143248084f5b791bdeef445738b
Sergey Aslanov <aslanovsergey@gmail.com>
2024-03-12 14:40:23 +0300

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// O(a2 + b)
+function anagrams(stringA, stringB) {
+  const stringALetters = stringA.replace(/[^a-zA-Z]+/, "").toLowerCase();
+  const stringBLetters = stringB.replace(/[^a-zA-Z]+/, "").toLowerCase();
+
+  if (stringALetters.length !== stringBLetters.length) {
+    return false;
+  }
+
+  const charsMapA = {};
+  const charsMapB = {};
+
+  for (const char of stringA) {
+    charsMapA[char] = (charsMapA[char] || 0) + 1;
+  }
+
+  for (const char of stringB) {
+    charsMapB[char] = (charsMapB[char] || 0) + 1;
+  }
+
+  for (const key in charsMapA) {
+    if (charsMapA[key] !== charsMapB[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
+
+// Another solution could to sort and then compare strings they must me equal

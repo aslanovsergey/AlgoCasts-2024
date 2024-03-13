8afdf2498daa077ce14328cba40ca446cfc6b568
Sergey Aslanov <aslanovsergey@gmail.com>
2024-03-13 17:42:19 +0300

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const pattern = /[^aeiou]/gi;
+
+  return str.replace(pattern, "").length;
+}
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   const vowels = "aeiou".split("");
+
+//   return str.split("").filter((c) => vowels.includes(c.toLowerCase())).length;
+// }

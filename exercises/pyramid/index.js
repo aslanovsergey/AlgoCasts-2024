50f2e3c3e1f4fc5c180b7a9451df29f7f8cfeb70
Sergey Aslanov <aslanovsergey@gmail.com>
2024-03-13 15:40:54 +0300

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,67 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, level = 1, length = 0) {
+  if (n === 0) {
+    return "";
+  }
+
+  const strLength = length || n * 2 - 1;
+
+  const hashLength = level * 2 - 1;
+  const gapLength = (strLength - hashLength) / 2;
+
+  const gap = " ".repeat(gapLength);
+  const str = gap + "#".repeat(hashLength) + gap;
+
+  console.log(str);
+
+  if (n > level) {
+    pyramid(n, level + 1, strLength);
+  }
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   if (n === 1) {
+//     console.log("#");
+//   }
+
+//   const strLength = n * 2 - 1;
+
+//   for (let row = 1; row <= n; row++) {
+//     const hashLength = row * 2 - 1;
+//     const gapLength = (strLength - hashLength) / 2;
+
+//     const gap = " ".repeat(gapLength);
+
+//     const str = gap + "#".repeat(hashLength) + gap;
+
+//     console.log(str);
+//   }
+// }
+
+// function pyramid(n) {
+//     if (n === 0) {
+//       return "";
+//     }
+
+//     const strLength = n * 2 - 1;
+
+//     printLevel(strLength, 1);
+//   }
+
+//   function printLevel(strLength, level) {
+//     const hashLength = level * 2 - 1;
+//     const gapLength = (strLength - hashLength) / 2;
+
+//     const gap = " ".repeat(gapLength);
+//     const str = gap + "#".repeat(hashLength) + gap;
+
+//     console.log(str);
+
+//     if (strLength > hashLength) {
+//       printLevel(strLength, level + 1);
+//     }
+//   }

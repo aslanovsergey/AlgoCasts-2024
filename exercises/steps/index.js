611745c7c357eb1b8bad75105cbd770d09047927
Sergey Aslanov <aslanovsergey@gmail.com>
2024-03-13 13:52:03 +0300

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  printStep(n, n);
+}
+
+function printStep(columns, currentRow) {
+  if (currentRow > 1) {
+    printStep(columns, currentRow - 1);
+  }
+
+  const str = "#".repeat(currentRow) + " ".repeat(columns - currentRow);
+
+  console.log(str);
+}
 
 module.exports = steps;
+
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//       let str = "";
+//       for (let column = 0; column < n; column++) {
+//         if (row >= column) {
+//           str += "#";
+//         } else {
+//           str += " ";
+//         }
+//       }
+
+//       console.log(str);
+//     }
+//   }
+
+// function steps(n) {
+//     printStep(n, 1);
+//   }
+
+//   function printStep(columns, currentRow) {
+//     const str = "#".repeat(currentRow) + " ".repeat(columns - currentRow);
+
+//     console.log(str);
+
+//     if (currentRow < columns) {
+//       printStep(columns, currentRow + 1);
+//     }
+//   }
